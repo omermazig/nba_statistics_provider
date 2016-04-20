0312c21329899c58eccb0ddbeee02912f3d9ec16
omer mazig <omermazig@hotmail.com>
2016-04-20 13:52:14 +0300

    syntax

--- New folder/script_nba.py
+++ New folder/script_nba.py
@@ -1,22 +1,21 @@
 import sys
 import os
+import pickle
+import IPython
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(path)
 from playerScripts import NBAPlayer
 from teamScripts import NBATeam
-from leagueScripts import NBALeague
+from leagueScripts import NBALeague, league_object_pickle_path_regex
 from gameScripts import NBAGame, NBASingleSeasonGames
 
 sys.path.remove(path)
 import goldsberry
-import pickle
-import IPython
-import sys
 
 if __name__ == "__main__":
-    # with open(r"C:\Users\Administrator\Documents\NBA\pythonProjects\pythonPickles\nba_players_objects_2015.pickle",
-    #           "rb") as file1:
-    #     player_objects_2015 = pickle.load(file1)
-    # sys.path.append(r'C:\Users\Administrator\Documents\NBA\pythonProjects')
+    with open(league_object_pickle_path_regex.format(season='2015'),
+              "rb") as file1:
+        player_objects_2015 = pickle.load(file1)
+    sys.path.append(r'C:\Users\Administrator\Documents\NBA\pythonProjects')
     IPython.embed()
