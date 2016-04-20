add9b6d3e5b6abb581b388b1da31808305883aaa
omer mazig <omermazig@hotmail.com>
2016-04-20 15:39:10 +0300

    moved get_cached_league_object to function at utilsScripts

--- New folder/script_nba.py
+++ New folder/script_nba.py
@@ -9,6 +9,7 @@ from playerScripts import NBAPlayer
 from teamScripts import NBATeam
 from leagueScripts import NBALeague, league_object_pickle_path_regex
 from gameScripts import NBAGame, NBASingleSeasonGames
+from utilsScripts import get_cached_league_object
 
 sys.path.remove(path)
 import goldsberry
