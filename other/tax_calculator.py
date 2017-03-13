fa485ac4f7769fd6ace15f80b042715d04c10541
omer mazig <omermazig@hotmail.com>
2017-03-13 23:46:22 +0200

    * Improved tax calculator

--- other/tax_calculator.py
+++ other/tax_calculator.py
@@ -1,6 +1,16 @@
-luxury_tax = 84.74 * 1000000
-nonrepeater_list1 = [(luxury_tax, 0), (5000000, 1.5), (5000000, 1.75), (5000000, 2.5), (5000000, 3.25), (5000000, 3.75), (5000000, 4.25), (5000000, 4.75)]
-repeater_list1 = [(luxury_tax, 0), (5000000, 2.5), (5000000, 2.75), (5000000, 3.5), (5000000, 4.25), (5000000, 4.75), (5000000, 5.25), (5000000, 5.75)]
+salary_cap_2016 = 94 * 1000000
+luxury_tax_2016 = 84.74 * 1000000
+
+salary_cap_2017 = 102 * 1000000
+luxury_tax_2017 = 113.287 * 1000000
+
+salary_cap_2018 = 108 * 1000000
+
+nonrepeater_list1 = [(luxury_tax_2017, 0), (5000000, 1.5), (5000000, 1.75), (5000000, 2.5), (5000000, 3.25),
+                     (5000000, 3.75), (5000000, 4.25), (5000000, 4.75)]
+repeater_list1 = [(luxury_tax_2017, 0), (5000000, 2.5), (5000000, 2.75), (5000000, 3.5), (5000000, 4.25),
+                  (5000000, 4.75), (5000000, 5.25), (5000000, 5.75)]
+
 
 def get_taxes(salaries, repeater=False):
     temp_salaries = salaries
@@ -15,20 +25,21 @@ def get_taxes(salaries, repeater=False):
             temp_salaries -= tuple1[0]
     return taxes
 
-cavs_salaries = 94.853699 * 1000000
-tristan_qo_salary = 6.777589 * 1000000
-tristan_salary = 16.407500 * 1000000
-jr_salary = 5 * 1000000
 
-print get_taxes(cavs_salaries)
-print get_taxes(cavs_salaries + tristan_salary)
-print get_taxes(cavs_salaries + tristan_qo_salary)
-print get_taxes(cavs_salaries + tristan_qo_salary)
+def get_bill_with_taxes(salaries, repeater=False):
+    return salaries + get_taxes(salaries, repeater=repeater)
 
 
-def calculate_max_salary(max_years=5, maxi=16407500, jump=1.075, salary=0, year=1):
+def calculate_max_salary(maxi, max_years=5, jump=1.075, salary=0, year=1):
     if year > max_years:
         return salary
     else:
-        salary += maxi * pow(jump, (year-1))
-        return calculate_max_salary(max_years, maxi, jump, salary, year+1)
+        salary += maxi * pow(jump, (year - 1))
+        return calculate_max_salary(maxi, max_years, jump, salary, year + 1)
+
+cavs_salaries_without_jr = 116.599976 * 1000000
+jr_salary = 15 * 1000000
+
+
+print(get_bill_with_taxes(cavs_salaries_without_jr, repeater=True))
+print(get_bill_with_taxes(cavs_salaries_without_jr+jr_salary, repeater=True))
